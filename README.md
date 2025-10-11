1e892325efff6edfff60c92e6a685e5a39ee5b16
andrescheffer28 <anddrecosttascheffer@gmail.com>
2025-10-11 16:06:34 -0300

    add: tests e2e database

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-npm i @nestjs/passport
-npm @nestjs/jwt
-npm i passport-jwt
-npm i passport-jwt
+- npm i @nestjs/passport
+- npm @nestjs/jwt
+- npm i passport-jwt
+- npm i passport-jwt
+
+- npm i --save-dev vitest unplugin-swc @swc/core @vitest/coverage-v8
+- npm i vite-tsconfig-paths -D
+- npm i dotenv -D
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import swc from 'unplugin-swc'
-import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
@@ -8,9 +8,9 @@ export default defineConfig({
     root: './',
   },
   plugins: [
-    tsconfigPaths(),
+    tsConfigPaths(),
     swc.vite({
       module: { type: 'es6' },
     }),
   ],
-})
+});
