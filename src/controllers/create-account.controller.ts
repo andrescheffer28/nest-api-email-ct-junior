272b9908327031f99192c0a8a3aefeac3301945d
andrescheffer28 <anddrecosttascheffer@gmail.com>
2025-10-11 06:17:15 -0300

    add: create account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,51 @@
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe';
+import { PrismaService } from '@/prisma/prisma.service';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import z from 'zod';
+
+export const createAccountSchema = z
+  .object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string(),
+  })
+  .required();
+
+type CreateAccount = z.infer<typeof createAccountSchema>;
+
+@Controller('/login')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountSchema))
+  async handle(@Body() body: CreateAccount) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      );
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  }
+}
